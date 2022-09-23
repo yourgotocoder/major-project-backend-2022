13b9724a32b38662c61549e99bcbdbadd4932da4
yourgotocoder <yourgotocoder@gmail.com>
2022-09-23 14:19:26 +0530

    Initialized the server

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.listen(process.env.PORT, () => console.log(`Server started on port: ${process.env.PORT}`));
