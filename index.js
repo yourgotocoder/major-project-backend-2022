2dcb071898f122be32a0ea7b752bbcfd38eccf2b
yourgotocoder <yourgotocoder@gmail.com>
2022-09-23 14:22:40 +0530

    README initialized

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
+app.get("")
+
 app.listen(process.env.PORT, () => console.log(`Server started on port: ${process.env.PORT}`));
